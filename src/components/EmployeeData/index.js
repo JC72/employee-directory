2a87904cec08b036dd3624df5a22cdb69b0712f1
Jeff Clegg <jeffclegg@att.net>
2021-02-09 23:20:44 -0500

    fixed functionality of Table component

---
+++ src/components/EmployeeData/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Moment from "react-moment";
+
+function EmployeeData (props) {
+    return(
+        <div>
+            <tbody>
+                {props.state.filteredEmployees.map((employee) => {
+                const { first, last } = employee.name;
+                const fullName = `${first} ${last}`;
+
+                // Format date
+                const dob = props.formatDate(employee.dob.date);
+
+                return (
+                    <tr key={employee.login.uuid}>
+                    <td>
+                        <img src={employee.picture.thumbnail} alt={fullName} />
+                    </td>
+                    <td className="align-middle">{fullName}</td>
+                    <td className="align-middle">
+                    <a href={`tel:+1${employee.phone}`}>{employee.phone}</a></td>
+                    <td className="align-middle email">
+                        <a href={`mailto:${employee.email}`}>{employee.email}</a>
+                    </td>
+                    <Moment format = "MM/DD/YYYY">
+                    <td className="align-middle">{dob}</td>
+                    </Moment>
+                    </tr>
+                );
+                })}
+            </tbody>
+        </div>
+    )
+}
+export default EmployeeData;
