2d89dc600bf4655f8c6c7740650726644f50b164
Jeff Clegg <jeffclegg@att.net>
2021-02-09 23:21:44 -0500

    adjusted style of header

---
+++ src/components/MainHeader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+        return (
+            <div>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="text-center text-black">Employee Directory</h1>
+                        <p className="lead text-center text-black">Click on category heading or use the search box to narrow results.</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+export default Header;
